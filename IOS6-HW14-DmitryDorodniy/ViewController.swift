21ab6d9403265f79b0b3c8fc0cfbe73599ad4ab0
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-06-07 13:54:35 +0300

    Initial Commit

---
+++ IOS6-HW14-DmitryDorodniy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS6-HW14-DmitryDorodniy
+//
+//  Created by Dmitry Dorodniy on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
